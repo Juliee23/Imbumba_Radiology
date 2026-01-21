b636f7740fc9a5ed5abcfc017277967c4563e2fd
Juliet Akani Ngomani <119965940+Juliee23@users.noreply.github.com>
2026-01-22 00:14:24 +0200

    fixed settings.py for deployment

--- imbumba/imbumba/settings.py
+++ imbumba/imbumba/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = ['https://imbumba-radiology.onrender.com','localhost','127.0.0.1
 # Application definition
 
 INSTALLED_APPS = [
+    'imbumba.home', 
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home', 
+    
 ]
 
 MIDDLEWARE = [
